708bf3f403d661c459d5b7d24f34bc92d26647e8
Jeremy Stashewsky <jeremy@goinstant.com>
2013-11-29 14:32:46 -0800

    Use absolute class names in package-info

--- src/main/java/com/goinstant/auth/package-info.java
+++ src/main/java/com/goinstant/auth/package-info.java
@@ -6,13 +6,15 @@
  * and Auth</a>. See the main <a href="https://goinstant.com">GoInstant</a>
  * website for what GoInstant is.
  * <br>
- * A {@link User} belongs to zero or more {@link Group}s.  These are both
- * declared as Interfaces so that you can tack them on easily to your existing
- * code via adapters.  For concrete implementations, use instead
- * {@link PlainUser} and {@link PlainGroup}.
+ * A {@link com.goinstant.auth.User} belongs to zero or more {@link
+ * com.goinstant.auth.Group}s.  These are both declared as Interfaces so that
+ * you can tack them on easily to your existing code via adapters.  For
+ * concrete implementations, use instead {@link com.goinstant.auth.PlainUser}
+ * and {@link com.goinstant.auth.PlainGroup}.
  * <br>
- * The {@link Signer} class is where the action happens.  You constrct it with
- * your GoInstant secret key, pass in a {@link User}, and BOOM, out pops a
- * token, ready for passing into the GoInstant client!
+ * The {@link com.goinstant.auth.Signer} class is where the action happens.
+ * You constrct it with your GoInstant secret key, pass in a {@link
+ * com.goinstant.auth.User}, and BOOM, out pops a token, ready for passing into
+ * the GoInstant client!
  */
 package com.goinstant.auth;
